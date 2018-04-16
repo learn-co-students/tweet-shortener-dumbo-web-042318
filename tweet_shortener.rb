cfd432c19b841112ca74005a216944c2881fba68
Daiane Bushey <daianebushey@gmail.com>
2018-04-16 19:28:32 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+  dictionary = {"hello" => "hi"
+                "to" => "2"
+                "two" => "2"
+                "too" => "2"
+                "for" => "4"
+                "four" => "4"
+                "be" => "b"
+                "you" => "u"
+                "at" => "@"
+                "and" => "&"}
+end
+
+def word_substituter(str)
+  dic = dictionary
+  final_str = ""
+  arr = str.split(" ")
+
+  binding.pry
+
+  # arr.each do |w|
+  #   if w ==
+  #
+  # end
+
+
+  #final_str
+end
